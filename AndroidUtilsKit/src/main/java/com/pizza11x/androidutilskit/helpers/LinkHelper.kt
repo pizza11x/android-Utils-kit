7caf834561d7aa45feece2e8b24432dba53dbd38
Antonio Sarno <>
2022-11-15 01:06:12 +0100

    LinkHelper

---
+++ AndroidUtilsKit/src/main/java/com/pizza11x/androidutilskit/helpers/LinkHelper.kt
@@ -0,0 +1,14 @@
+package com.pizza11x.androidutilskit.helpers
+
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+
+object LinkHelper {
+
+    fun openExternalBrowser(context: Context, url: String) {
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.data = Uri.parse(url)
+        context.startActivity(intent)
+    }
+}
